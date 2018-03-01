ca062bbe16a5f34c38a0ce5e6db7fcd4ea963317
Kuty <quentin.le-baron@epitech.eu>
2018-03-01 19:23:33 +0100

    Title: parss summary Ref:no ref Author: kuty Feature: base Description:
    	nothing

---
+++ utils/__init__.py
@@ -0,0 +1,15 @@
+class parssFile:
+
+    def __init__(self, filename):
+        with open(filename) as f:
+            read_data = f.read()
+            lines = read_data.split("\n")
+            summary = lines[0].split(" ")
+            print(summary)
+            self.__summary = dict({"row": int(summary[0]),
+                                   "col": int(summary[1]),
+                                   "car": int(summary[2]),
+                                   "rides": int(summary[3]),
+                                   "steps": int(summary[5])})
+
+            print(self.__summary)
