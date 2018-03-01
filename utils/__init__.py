def1e66581e929ee4da4c458e7a0c5d832de18d5
Kuty <quentin.le-baron@epitech.eu>
2018-03-01 19:58:23 +0100

    class cars Ref: Author: Feature: Description:

--- utils/__init__.py
+++ utils/__init__.py
@@ -14,11 +14,27 @@ class Rides:
         return self.__conf
 
 
+class Care:
+
+    def __init__(self):
+        self.__x = 0
+        self.__y = 0
+        self.__stauts = 0
+
+    def move(self, position):
+        self.__x = position["x"]
+        self.__y = position["y"]
+
+    def get_position(self):
+        return dict({"x": self.__x, "y": self.__y})
+
+
 class parssFile:
 
     def __init__(self, filename):
         self.__summary = set()
         self.__rides = []
+        self.__cars = []
         with open(filename) as f:
             read_data = f.read()
             for i, line in enumerate(read_data.split("\n")):
@@ -31,12 +47,17 @@ class parssFile:
                                            "rides": int(summary[3]),
                                            "steps": int(summary[5])})
 
+                    for i in range(self.__summary["car"]):
+                        self.__cars.append(Care())
                 else:
                     if line != "":
                         self.__rides.append(Rides(line))
 
     def get_summary(self):
         return self.__summary
+
+    def get_cars(self):
+        return self.__cars
 
     def get_rides(self):
         return self.__rides
